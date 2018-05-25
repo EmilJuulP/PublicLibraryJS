8fe2705ce839fa7f796525f30f75793f22b89386
Emil Juul Petersen <emil022n@edu.campusvejle.dk>
2018-05-25 09:34:54 +0200

    First commit

---
+++ src/model/Book.js
@@ -0,0 +1,26 @@
+function Book(slots) {
+    this.isbn = slots.isbn;
+    this.title = slots.title;
+    this.year = slots.year;
+};
+
+Book.loadAll = function () {
+    var i=0, key="", keys=[], bookTableString="", bookTable={};
+    try {
+        if (localStorage["bookTable"]) {
+            bookTableString = localStorage["bookTable"];
+        }
+    }
+    catch (e) {
+        alert ("Error when reading from Local Storage\n" + e);
+    }
+    if (bookTableString) {
+        bookTable = JSON.parse(bookTableString);
+        keys = Object.keys(bookTable);
+        console.log( keys.length + " books loaded.");
+        for (i=0; i < keys.length; i++) {
+            key = keys[i];
+            Book.instances[key] = Book.convertRow20bj( bookTable[key]);
+        }
+    }
+};
