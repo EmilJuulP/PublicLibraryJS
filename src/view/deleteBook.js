7b3f96370c7f3e6ff7a92d6de49e13790b59e2f5
Emil Juul Petersen <emil022n@edu.campusvejle.dk>
2018-05-30 11:44:59 +0200

    fixed a bug in deleteBook.js

--- src/view/deleteBook.js
+++ src/view/deleteBook.js
@@ -1,32 +1,32 @@
 pl.view.deleteBook = {
     setupUserInterface: function () {
-        var deleteButton = document.forms['Book'].commit;
-        var selectE1 = document.forms['Book'].selectBook;
-        var i=0, key="", keys=[], Book=null, optionE1=null;
-        // Load all book objects
-        Book.loadAll();
-        keys = Object.keys( Book.instances);
-        // populate the selection list with books
-        for (i=0; i < keys.length; i++) {
-            key = keys[i];
-            book = Book.instances[key];
-            optionE1 = document.createElement("option");
-            optionE1.text = book.title;
-            optionE1.value = book.isbn;
-            selectE1.add( optionE1, null);
-        }
-        deleteButton.addEventListener("click",
-            pl.view.deleteBook.handleDeleteButtonClickEvent);
-        window.addEventListener("beforeunload", function () {
-            Book.saveAll();
-        });
+      var deleteButton = document.forms['Book'].commit;
+      var selectEl = document.forms['Book'].selectBook;
+      var i=0, key="", keys=[], book=null, optionEl=null;
+      // load all book objects
+      Book.loadAll();
+      keys = Object.keys( Book.instances);
+      // populate the selection list with books
+      for (i=0; i < keys.length; i++) {
+        key = keys[i];
+        book = Book.instances[key];
+        optionEl = document.createElement("option");
+        optionEl.text = book.title;
+        optionEl.value = book.isbn;
+        selectEl.add( optionEl, null);
+      }
+      deleteButton.addEventListener("click", 
+          pl.view.deleteBook.handleDeleteButtonClickEvent);
+      window.addEventListener("beforeunload", function () {
+          Book.saveAll(); 
+      });
     },
-    handleDeleteButtonClickEvent: function() {
-        var selectE1 = document.forms['Book'].selectBook;
-        var isbn = selectE1.value;
-        if (isbn) {
-            Book.destroy( isbn);
-            selectE1.remove( selectE1.selectedIndex);
-        }
+    handleDeleteButtonClickEvent: function () {
+      var selectEl = document.forms['Book'].selectBook;
+      var isbn = selectEl.value;
+      if (isbn) {
+        Book.destroy( isbn);
+        selectEl.remove( selectEl.selectedIndex);
+      }
     }
-};
+  };
